1b72472e7bcec272183d4eba222ab9f3b0cae7c2
袁浩 <yuanhaozhuzhu@hotmail.com>
2024-08-15 20:08:16 +0800

    fixemp

--- models/emp.go
+++ models/emp.go
@@ -9,7 +9,7 @@ type Emp struct {
 	orm.Model
 	Name     string `gorm:"column:name;not null" json:"name" form:"name"`
 	Email    string `gorm:"column:email;not null;unique_index:users_email_unique" json:"email" form:"email"`
-	Password string `gorm:"column:password;not null" json:"-" form:"password"` // Exclude password from JSON response
+	Password string `gorm:"column:password;not null" json:"password" form:"password"` // Exclude password from JSON response
 	WorkNo   string `gorm:"column:workno;not null;unique_index:users_workno_unique" json:"workno" form:"workno"`
 	DeptID   int    `gorm:"column:dept_id;not null;default:0" json:"dept_id" form:"dept_id"`
 	Leave    int    `gorm:"column:leave;not null;default:0" json:"leave" form:"leave"`
