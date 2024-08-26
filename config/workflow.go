4a4e0c4444f8605e2bbf7a9bd20e27cade69854c
袁浩 <yuanhaozhuzhu@hotmail.com>
2024-08-26 11:27:00 +0800

    udpate config

--- config/workflow.go
+++ config/workflow.go
@@ -7,7 +7,7 @@ import (
 func init() {
 	config := facades.Config()
 	config.Add("workflow", map[string]any{
-		"Dept": "Department", //部门关联应用中的模型
-		"Emp":  "User",       //员工关联应用中的模型
+		"Dept": "Dept", //部门关联应用中的模型
+		"Emp":  "User", //员工关联应用中的模型
 	})
 }
