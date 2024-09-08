4ad1ad3550a4ec7db34be56fcf901dfa2bc8295c
hulutech-web <yuanhaozhuzhu@hotmail.com>
2024-09-08 23:52:19 +0800

    设置临时的app_debug用于自定义迁移

--- services/workflow/official_plugins/orm.go
+++ services/workflow/official_plugins/orm.go
@@ -17,11 +17,11 @@ var gormIns *gormio.DB
 
 func BootMS() *gormio.DB {
 	once.Do(func() {
-		facades.Config().Add("Logger", 1)
-		var gormImpl = gorm.NewGormImpl(facades.Config(), "mysql", db.NewConfigImpl(facades.Config(), "mysql"), gorm.NewDialectorImpl(facades.Config(), "mysql"))
+		facades.Config().Add("APP_DEBUG", false)
+		var gormImpl = gorm.NewGormImpl(facades.Config(), "mysql",
+			db.NewConfigImpl(facades.Config(), "mysql"),
+			gorm.NewDialectorImpl(facades.Config(), "mysql"))
 		gormIns, _ = gormImpl.Make()
-		//关闭打印sql语句
-		gormIns.Logger.LogMode(2)
 	})
 	return gormIns
 }
